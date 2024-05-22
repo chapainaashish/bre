460de8448272e89669bf3aad2b7421e00c920fc1
Aashish Chapain <ashishchapain86@gmail.com>
2024-05-22 16:03:39 +0545

    migrated to mysql

--- listygo/settings.py
+++ listygo/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = "listygo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "property",
+        "USER": "admin",
+        "PASSWORD": "71904638930",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
